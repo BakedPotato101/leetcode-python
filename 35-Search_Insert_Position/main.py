957d2f88a200a00348f6489173ee264fd11abc69
BakedPotato101 <jackson.baker7562@gmail.com>
2023-06-14 17:46:17 -0500

    Probably a cheat but okay

---
+++ 35-Search_Insert_Position/main.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target in nums:
+            return nums.index(target)
+        else:
+            nums.append(target)
+            nums.sort()
+            return nums.index(target)
+                
+
+            
+            
+                        
+
+        
+s = Solution()
+print(s.searchInsert([1,2,4,5],3))
